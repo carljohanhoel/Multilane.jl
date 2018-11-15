9a98f07b2b3be68c8413ac692f75deeccdc6cfa4
carljohanhoel <carl-johan.hoel@chalmers.se>
2018-11-15 20:37:25 +0100

    Added stuff to the nn visualization video

--- scratch_cj/visualize_value_action.jl
+++ scratch_cj/visualize_value_action.jl
@@ -1,43 +1,30 @@
-# Need to run rerun_trained:model.jl first
+# Need to run rerun_trained_model.jl first
 
 
 #Visualization
 #Set time t used for showing tree. Use video to find interesting situations.
-t = 30.
+t = 0.0
 step = convert(Int, t / pp.dt) + 1
+
 state_t = deepcopy(hist.state_hist[step])
-
-
-state_t_saved = deepcopy(state_t)
-s_ego_veh = state_t.cars[1]
-v0 = zeros(7,1)
-p0_vec = zeros(7,5)
-for i in 1:7
-    y = i*0.5 + 0.5
-    s_ego_veh = CarState(s_ego_veh.x, y, s_ego_veh.vel, (mod(y,1.0)>0)*1.0, s_ego_veh.behavior, s_ego_veh.id)
-    state_t.cars[1] = s_ego_veh
-    v0[i] = estimate_value(planner.solved_estimate, planner.mdp, state_t, planner.solver.depth)[1]
-
-    allowed_actions = actions(planner.mdp, state_t)   #This is handled a bit weird to make it compatible with existing structure of Multilane.jl
-    all_actions = actions(planner.mdp)
-    if length(allowed_actions) == length(all_actions)
-        allowed_actions_vec = ones(Float64, 1, length(all_actions))
-    else
-        allowed_actions_vec = zeros(Float64, 1, length(all_actions))
-        for idx in collect(allowed_actions.acceptable)
-            allowed_actions_vec[idx] = 1.0
-        end
+allowed_actions = actions(planner.mdp, state_t)   #This is handled a bit weird to make it compatible with existing structure of Multilane.jl
+all_actions = actions(planner.mdp)
+if length(allowed_actions) == length(all_actions)
+    allowed_actions_vec = ones(Float64, 1, length(all_actions))
+else
+    allowed_actions_vec = zeros(Float64, 1, length(all_actions))
+    for idx in collect(allowed_actions.acceptable)
+        allowed_actions_vec[idx] = 1.0
     end
-    p0_vec[i,:] = MCTS.init_P(planner.solver.init_P, planner.mdp, state_t, allowed_actions_vec)
 end
+p_nn = MCTS.init_P(planner.solver.init_P, planner.mdp, state_t, allowed_actions_vec)
+v_nn = estimate_value(planner.solved_estimate, planner.mdp, state_t, planner.solver.depth)[1]
+p_tree = hist.ainfo_hist[step][:action_distribution]
+@show p_tree
+@show p_nn
+@show v_nn
 
 
-# p0_vec = MCTS.init_P(planner.solver.init_P, planner.mdp, state_t, allowed_actions_vec)
-# v0 = estimate_value(planner.solved_estimate, planner.mdp, state_t, planner.solver.depth)[1]
-
-
-
-write_to_png(visualize_with_nn(sim_problem,state_t,hist.reward_hist[step],v0,p0_vec),"./Figs/options.png")
 print(hist.action_hist[step])
 inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
 # inchromium(D3Tree(hist.ainfo_hist[step][:tree],hist.state_hist[step],init_expand=1))   #For MCTS (not DPW)
@@ -48,19 +35,34 @@ inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
 
 hist_copy = deepcopy(hist)
 
-frames = Frames(MIME("image/png"), fps=10/pp.dt)
-@showprogress for (s, ai, r, sp) in eachstep(hist_copy, "s, ai, r, sp")
-    s_ego_veh = s.cars[1]
+frames = Frames(MIME("image/png"), fps=3/pp.dt)
+step = 0
+@showprogress for (s, a, r, sp) in eachstep(hist_copy, "s, a, r, sp")
+    s_ego_veh = deepcopy(s.cars[1])
     s_ego_veh_original = deepcopy(s_ego_veh)
-    v0 = zeros(7,1)
-    p0_vec = zeros(7,5)
-    for i in 1:7
-        y = i*0.5 + 0.5
-        s_ego_veh = CarState(s_ego_veh.x, y, s_ego_veh.vel, (mod(y,1.0)>0)*1.0, s_ego_veh.behavior, s_ego_veh.id)
-        s.cars[1] = s_ego_veh
-        v0[i] = estimate_value(planner.solved_estimate, planner.mdp, s, planner.solver.depth)[1]
+    v0 = []
+    p0_vec = []
+    p0_vec_all_actions = []
 
-        allowed_actions = actions(planner.mdp, s)   #This is handled a bit weird to make it compatible with existing structure of Multilane.jl
+    s_vec = []
+    for y in 1:0.5:4
+        if mod(y,1.0)==0.
+            s_ego_veh = CarState(s_ego_veh.x, y, s_ego_veh.vel, 0.0, s_ego_veh.behavior, s_ego_veh.id)
+            s.cars[1] = s_ego_veh
+            push!(s_vec,deepcopy(s))
+        else
+            for lane_change in [-1,1.]
+                s_ego_veh = CarState(s_ego_veh.x, y, s_ego_veh.vel, lane_change, s_ego_veh.behavior, s_ego_veh.id)
+                s.cars[1] = s_ego_veh
+                push!(s_vec,deepcopy(s))
+            end
+        end
+    end
+
+    for state in s_vec
+        push!(v0, estimate_value(planner.solved_estimate, planner.mdp, state, planner.solver.depth)[1])
+
+        allowed_actions = actions(planner.mdp, state)   #This is handled a bit weird to make it compatible with existing structure of Multilane.jl
         all_actions = actions(planner.mdp)
         if length(allowed_actions) == length(all_actions)
             allowed_actions_vec = ones(Float64, 1, length(all_actions))
@@ -70,11 +72,29 @@ frames = Frames(MIME("image/png"), fps=10/pp.dt)
                 allowed_actions_vec[idx] = 1.0
             end
         end
-        p0_vec[i,:] = MCTS.init_P(planner.solver.init_P, planner.mdp, s, allowed_actions_vec)
+        push!(p0_vec, MCTS.init_P(planner.solver.init_P, planner.mdp, state, allowed_actions_vec))
+        #Raw estimation, without removing forbidden actions
+        converted_state = MCTS.convert_state(state, planner.mdp)
+        dist, value = estimator.py_class[:forward_pass](converted_state)
+        push!(p0_vec_all_actions, dist)
     end
     s.cars[1] = s_ego_veh_original
 
-    push!(frames, visualize_with_nn(sim_problem,s,r,v0,p0_vec))
+    allowed_actions = actions(planner.mdp, s)   #This is handled a bit weird to make it compatible with existing structure of Multilane.jl
+    all_actions = actions(planner.mdp)
+    if length(allowed_actions) == length(all_actions)
+        allowed_actions_vec = ones(Float64, 1, length(all_actions))
+    else
+        allowed_actions_vec = zeros(Float64, 1, length(all_actions))
+        for idx in collect(allowed_actions.acceptable)
+            allowed_actions_vec[idx] = 1.0
+        end
+    end
+    p_nn = MCTS.init_P(planner.solver.init_P, planner.mdp, s, allowed_actions_vec)
+    step += 1
+    p_tree = hist.ainfo_hist[step][:action_distribution]
+
+    push!(frames, visualize_with_nn(sim_problem,s,a,r,v0,p0_vec, p0_vec_all_actions, p_nn, p_tree))
 end
-gifname = "./Figs/options_video.ogv"
-write(gifname, frames)
+gifname = "./Figs/options_video_"*network_to_load*sample_to_load*".ogv"
+write(gifname, frames)   #The bad quality probably has something to do with the creation of the video. The frames themselves look good. Also, with a smaller fps, it looks good.

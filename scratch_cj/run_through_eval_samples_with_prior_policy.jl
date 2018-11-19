d905407b1e039c41147282c6d3ea68b6d011249a
carljohanhoel <carl-johan.hoel@chalmers.se>
2018-11-19 16:54:38 +0100

    Fised problem with rng:s

--- scratch_cj/run_through_eval_samples_with_prior_policy.jl
+++ scratch_cj/run_through_eval_samples_with_prior_policy.jl
@@ -8,14 +8,10 @@ parallel_version = false
 simple_run = false
 tree_in_info = false
 
-# sample_to_load = "6081"
-# network_to_load = "181024_155209_driving_Change_pen_0p03_Loss_weights_1_100_Cpuct_0p1_Fixed_maxpool_Change_back_pen_0p03_Update_3_times_per_sample"
-# logs_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"
-
 ##
 #Code for loading saved networks, running them with only the NN policy and printing the result to a file
 logs_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"
-network_to_load = "181024_155209_driving_Change_pen_0p03_Loss_weights_1_100_Cpuct_0p1_Fixed_maxpool_Change_back_pen_0p03_Update_3_times_per_sample"
+network_to_load = "181116_155907_driving_Change_pen_0p03_Cpuct_0p1_Dpw_0p3_N_final_32_Lane_change_in_ego_state_V_min_10_No_batchnorm"
 eval_samples = []
 all_files = readdir(logs_path*network_to_load)
 old_results = ""
@@ -47,12 +43,15 @@ for sample_to_load in eval_samples
 
     ##
     for process in 3:22
-        #process = 7
+    # for process in 5:5
         i=process-2
-        rng_base_seed = 15
-        rng_seed = 100*(i-1)+rng_base_seed
-        rng = MersenneTwister(rng_seed)
-        s_initial = initial_state(sim_problem, rng, initSteps=initSteps)
+
+        # Reset rng:s
+        rng_evaluator_copy=MersenneTwister(Int(rng_evaluator.seed[1])+100*(i-1))
+        rng_history_copy=MersenneTwister(Int(rng_history.seed[1])+100*(i-1))
+        hr.rng = rng_history_copy
+
+        s_initial = initial_state(sim_problem, rng_evaluator_copy)
         s_initial = set_ego_behavior(s_initial, ego_acc)
         o_initial = MLObs(s_initial, problem.dmodel.phys_param.sensor_range, problem.dmodel.phys_param.obs_behaviors)
 
@@ -60,21 +59,21 @@ for sample_to_load in eval_samples
         if sim_problem isa POMDP
             if solver isa MLMPCSolver
                 updater = make_updater(cor, sim_problem, rng_seed)
-                planner = deepcopy(solve(solver, sim_problem))
-                srand(planner, rng_seed+60000)   #Sets rng seed of planner
+                planner = deepcopy(policy)
+                srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
                 planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
                 hist = simulate(hr, sim_problem, planner, updater, o_initial, s_initial)
             else
                 updater = LimitedRangeUpdater()
-                planner = deepcopy(solve(solver, sim_problem))
-                srand(planner, rng_seed+60000)   #Sets rng seed of planner
+                planner = deepcopy(policy)
+                srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
                 planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
                 hist = simulate(hr, sim_problem, planner, updater, o_initial, s_initial)
             end
         else
-            planner = deepcopy(solve(solver, sim_problem))
+            planner = deepcopy(policy)
             planner.solver.n_iterations = 1
-            srand(planner, rng_seed+60000)   #Sets rng seed of planner
+            srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
             planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
             hist = simulate(hr, sim_problem, planner, s_initial)
         end
@@ -82,30 +81,35 @@ for sample_to_load in eval_samples
         @show sum(hist.reward_hist)
         @show hist.state_hist[end].x
 
+        action_stats = zeros(1,5)
+        for action in hist.action_hist
+            if action == MLAction(0.0, 0.0, 1.0)
+                action_stats[1] += 1
+            elseif action == MLAction(-1.0, 0.0, 1.0)
+                action_stats[2] += 1
+            elseif action == MLAction(1.0, 0.0, 1.0)
+                action_stats[3] += 1
+            elseif action == MLAction(0.0, -1.0, 1.0)
+                action_stats[4] += 1
+            elseif action == MLAction(0.0, 1.0, 1.0)
+                action_stats[5] += 1
+            end
+        end
+
 
         open(logs_path*network_to_load*"/prior_policy_result.txt","a") do f
-            writedlm(f, [[i, sample_to_load, sum(hist.reward_hist), hist.state_hist[end].x]], " ")
+            writedlm(f, [[i, sample_to_load, sum(hist.reward_hist), hist.state_hist[end].x, action_stats[1], action_stats[2], action_stats[3], action_stats[4], action_stats[5] ]], " ")
         end
+
+
+        # #Produce video
+        # frames = Frames(MIME("image/png"), fps=3/pp.dt)
+        # @showprogress for (s, ai, r, sp) in eachstep(hist, "s, ai, r, sp")
+        #     push!(frames, visualize(problem, s, r))
+        # end
+        # gifname = "./Figs/nn_policy_process_"*string(process)*"_"*network_to_load*"_"*sample_to_load*"__.ogv"
+        # write(gifname, frames)
+
     end
 
 end
-
-#
-#
-# #Visualization
-# #Set time t used for showing tree. Use video to find interesting situations.
-# t = 0.0
-# step = convert(Int, t / pp.dt) + 1
-# write_to_png(visualize(sim_problem,hist.state_hist[step],hist.reward_hist[step]),"./Figs/state_at_t"*string(t)*"_"*network_to_load*"_"*sample_to_load*".png")
-# print(hist.action_hist[step])
-# inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
-# # inchromium(D3Tree(hist.ainfo_hist[step][:tree],hist.state_hist[step],init_expand=1))   #For MCTS (not DPW)
-#
-#
-# #Produce video
-# frames = Frames(MIME("image/png"), fps=10/pp.dt)
-# @showprogress for (s, ai, r, sp) in eachstep(hist, "s, ai, r, sp")
-#     push!(frames, visualize(problem, s, r))
-# end
-# gifname = "./Figs/rerunAZ_process_"*string(process)*"_"*network_to_load*"_"*sample_to_load*".ogv"
-# write(gifname, frames)

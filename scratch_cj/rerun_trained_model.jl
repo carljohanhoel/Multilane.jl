23d12e719f142714d685c62820a7835e7370dd79
carljohanhoel <carl-johan.hoel@chalmers.se>
2018-11-19 17:08:19 +0100

    Fixed problem with rng:s, although had just a small effect here

--- scratch_cj/rerun_trained_model.jl
+++ scratch_cj/rerun_trained_model.jl
@@ -12,44 +12,50 @@ simple_run = false
 
 tree_in_info = true
 
-sample_to_load = "6081"
-network_to_load = "181024_155209_driving_Change_pen_0p03_Loss_weights_1_100_Cpuct_0p1_Fixed_maxpool_Change_back_pen_0p03_Update_3_times_per_sample"
+sample_to_load = "4181"
+# network_to_load = "181016_140842_driving_Change_pen_0p01_Loss_weights_1_10_Cpuct_0p1_Remove_10_samples_Only_z_target_No_vehicles"
+network_to_load = "181115_205258_driving_Change_pen_0p03_Cpuct_0p1_Dpw_0p3_N_final_32_Lane_change_in_ego_state_V_min_10"
+
 logs_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"
 
 
 include("simulation_setup.jl")
 
+hr.max_steps = 10
 
 ##
 #Possibly add loop here, loop over process
-process = 7
+process = 5
 i=process-2
-rng_base_seed = 15
-rng_seed = 100*(i-1)+rng_base_seed
-rng = MersenneTwister(rng_seed)
-s_initial = initial_state(sim_problem, rng, initSteps=initSteps)
+
+# Reset rng:s (not necessary if not running a loop)
+rng_evaluator_copy=MersenneTwister(Int(rng_evaluator.seed[1])+100*(i-1))
+rng_history_copy=MersenneTwister(Int(rng_history.seed[1])+100*(i-1))
+hr.rng = rng_history_copy
+
+s_initial = initial_state(sim_problem, rng_evaluator_copy)
 s_initial = set_ego_behavior(s_initial, ego_acc)
 o_initial = MLObs(s_initial, problem.dmodel.phys_param.sensor_range, problem.dmodel.phys_param.obs_behaviors)
 
 ##
 if sim_problem isa POMDP
     if solver isa MLMPCSolver
-        updater = make_updater(cor, sim_problem, rng_seed)
+        updater = make_updater(cor, sim_problem, rng_seed+2+100*(i-1))
         planner = deepcopy(solve(solver, sim_problem))
-        srand(planner, rng_seed+60000)   #Sets rng seed of planner
+        srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
         planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
         hist = simulate(hr, sim_problem, planner, updater, o_initial, s_initial)
     else
         updater = LimitedRangeUpdater()
         planner = deepcopy(solve(solver, sim_problem))
-        srand(planner, rng_seed+60000)   #Sets rng seed of planner
+        srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
         planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
         hist = simulate(hr, sim_problem, planner, updater, o_initial, s_initial)
     end
 else
     planner = deepcopy(solve(solver, sim_problem))
-    # planner.solver.n_iterations = 1
-    srand(planner, rng_seed+60000)   #Sets rng seed of planner
+    planner.solver.n_iterations = 1
+    srand(planner, Int(policy.rng.seed[1])+100*(i-1))   #Sets rng seed of planner
     planner.training_phase = false   #Remove random action exploration, always choose the node that was most visited after the MCTS
     hist = simulate(hr, sim_problem, planner, s_initial)
 end
@@ -69,7 +75,7 @@ inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
 
 
 #Produce video
-frames = Frames(MIME("image/png"), fps=10/pp.dt)
+frames = Frames(MIME("image/png"), fps=3/pp.dt)
 @showprogress for (s, ai, r, sp) in eachstep(hist, "s, ai, r, sp")
     push!(frames, visualize(problem, s, r))
 end

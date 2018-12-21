a09a3038b71b0b7a6ea85365454798b93c78e00f
carljohanhoel <carl-johan.hoel@chalmers.se>
2018-12-21 09:39:18 +0100

    Added possibility of rerunning multiple runs. Save files to log folder

--- scratch_cj/rerun_trained_model.jl
+++ scratch_cj/rerun_trained_model.jl
@@ -4,8 +4,8 @@ push!(LOAD_PATH,joinpath("./src"))
 
 using Revise #To allow recompiling of modules withhout restarting julia
 
-@show scenario = "continuous_driving"
-# @show scenario = "exit_lane"
+# @show scenario = "continuous_driving"
+@show scenario = "exit_lane"
 
 # parallel_version = true   #Test code in parallel mode
 parallel_version = false
@@ -13,11 +13,19 @@ parallel_version = false
 # simple_run = true
 simple_run = false
 
-tree_in_info = true
+tree_in_info = false
+# tree_in_info = true
 
-sample_to_load = "10071"
-# network_to_load = "181016_140842_driving_Change_pen_0p01_Loss_weights_1_10_Cpuct_0p1_Remove_10_samples_Only_z_target_No_vehicles"
-network_to_load = "181119_180615_driving_Change_pen_0p03_Cpuct_0p1_Dpw_0p3_N_final_32_Lane_change_in_ego_state_V_min_10_Added_set_V_set_T_ego_state"
+# sample_to_load = "15011"
+sample_to_load = "14200"
+
+# network_to_load = "181119_180615_driving_Change_pen_0p03_Cpuct_0p1_Dpw_0p3_N_final_32_Lane_change_in_ego_state_V_min_10_Added_set_V_set_T_ego_state"
+# network_to_load = "181126_154437_driving_Cpuct_0p1_Dpw_0p3_V_min_10_Big_replay_Truck_dim"
+# network_to_load = "181126_155336_driving_Cpuct_0p1_Dpw_0p3_V_min_10_Big_replay_Truck_dim_Weights_1_10"
+
+# network_to_load = "181130_160730_driving_exit_lane_Cpuct_0p1_Dpw_0p3_Big_replay_Truck_dim"
+network_to_load = "181203_174746_driving_exit_lane_Cpuct_0p1_Dpw_0p3_Big_replay_Truck_dim_Terminal_state_Est_v0"
+
 
 logs_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"
 
@@ -26,10 +34,17 @@ include("simulation_setup.jl")
 
 # hr.max_steps = 10
 
+hist = []      #Needs to be defines so can us include visualize_value_action
+planner = []   #Needs to be defines so can us include visualize_value_action
+process = []   #Needs to be defines so can us include visualize_value_action
 ##
 #Possibly add loop here, loop over process
-process = 5
+# for process in 23:102
+# for process in 72:102
+process = 15
 i=process-2
+
+@show i
 
 # Reset rng:s (not necessary if not running a loop)
 rng_evaluator_copy=MersenneTwister(Int(rng_evaluator.seed[1])+100*(i-1))
@@ -69,12 +84,15 @@ end
 
 #Visualization
 #Set time t used for showing tree. Use video to find interesting situations.
+if !ispath(logs_path*network_to_load*"/Reruns")
+   mkdir(logs_path*network_to_load*"/Reruns")
+end
 t = 0.0
 step = convert(Int, t / pp.dt) + 1
-write_to_png(visualize(sim_problem,hist.state_hist[step],hist.reward_hist[step]),"./Figs/state_at_t"*string(t)*"_"*network_to_load*"_"*sample_to_load*".png")
-print(hist.action_hist[step])
-inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
-# inchromium(D3Tree(hist.ainfo_hist[step][:tree],hist.state_hist[step],init_expand=1))   #For MCTS (not DPW)
+# write_to_png(visualize(sim_problem,hist.state_hist[step],hist.reward_hist[step]),"./Figs/state_at_t"*string(t)*"_"*network_to_load*"_"*sample_to_load*".png")
+write_to_png(visualize(sim_problem,hist.state_hist[step],hist.reward_hist[step]),logs_path*network_to_load*"/Reruns/"*"sample_"*sample_to_load*"_process_"*string(process)*"_state_at_t_"*string(t)*"_"*network_to_load*".png")
+# print(hist.action_hist[step])
+# inchromium(D3Tree(hist.ainfo_hist[step][:tree],init_expand=1))
 
 
 #Produce video
@@ -82,5 +100,19 @@ frames = Frames(MIME("image/png"), fps=3/pp.dt)
 @showprogress for (s, ai, r, sp) in eachstep(hist, "s, ai, r, sp")
     push!(frames, visualize(problem, s, r))
 end
-gifname = "./Figs/rerunAZ_process_"*string(process)*"_"*network_to_load*"_"*sample_to_load*".ogv"
+gifname = logs_path*network_to_load*"/Reruns/"*"sample_"*sample_to_load*"_process_"*string(process)*"_rerun_"*network_to_load*".ogv"
 write(gifname, frames)
+
+
+#Vis options
+include("visualize_value_action.jl")
+
+
+#Save result to file
+log = MCTS.create_eval_log(sim_problem, hist, process, parse(Int,sample_to_load))
+open(logs_path*network_to_load*"/Reruns/"*"evalResults2_.txt","a") do f
+    writedlm(f, log, " ")
+end
+
+
+# end
